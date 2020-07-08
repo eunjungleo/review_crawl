cd9598e76a01cbea14cd61190b545ad8b74236c2
eunjungleo <asaprocky123@likelion.org>
2020-07-08 14:41:39 +0900

    all for today

--- gyno.py
+++ gyno.py
@@ -2,47 +2,55 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import csv
+import csv, time
 
 
 chromedriver = 'chromedriver.exe'
 driver = webdriver.Chrome(chromedriver)
 
-# get path to items
-get_path = 'div > div.cell_text._cell_text > div.area_text'
 
 # open page
 driver.get('https://smartstore.naver.com/liwoostore/products/3394361389#revw')
 
-
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "button_more_text")))
+page = driver.find_elements_by_css_selector('nav._review_list_page > nav > a.page.number')
+print(page)
 """
-# 더보기 버튼 펼치기
-load_btns = driver.find_elements_by_class_name('button_more_text')
-for i in range(1,20):
-    load_btns[i].execute_script("arguments[0].click();", element)
-   """ 
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "button_more_text")))
 
-# get review item
-# get_rvws = driver.find_elements_by_class_name('area_text')
+    # load more
+    load_btns = driver.find_elements_by_class_name('button_more_text')
+    for i in range(0,19):
+        load_btns[i].send_keys('\n')
 
-get_user_info = driver.find_elements_by_css_selector(get_path+'div:nth-child(2) > div > span:nth-child(1)')
-for user_info in get_user_info:
-    print(user_info.text)
+    # user id
+    get_user_id = driver.find_elements(By.CSS_SELECTOR, ".area_status_user > span:nth-child(1)")
+    for u in get_user_id:
+        print(u.text)
 
+    # date
+    get_date = driver.find_elements(By.CSS_SELECTOR, ".area_status_user > span:nth-child(2)")
+    for u in get_date:
+        print(u.text)
+
+    # product_option
+    get_option = driver.find_elements(By.CSS_SELECTOR, ".area_status_user > p.text_info.text_option")
+    for u in get_option:
+        print(u.text)
+
+    # comment body
+    get_body = driver.find_elements(By.CSS_SELECTOR, ".review_text._review_text")
+    for u in get_body:
+        print(u.text)
+
+    # stars
+    get_stars = driver.find_elements(By.CSS_SELECTOR, ".number_grade")
+    for u in get_stars:
+        print(u.text)
+
+    # comment likes 
+    get_comment_likes = driver.find_elements(By.CSS_SELECTOR, "span.count._count")
+    for u in get_comment_likes:
+        print(u.text)
 """
-# get data
-pages = driver..execute_script('setAttribute(find_elements_by_class_name('page.number'),"area-selected","true");)')
 
-# comment_id = 
-user_id = driver.find_elements_by_css_selector('div.cell_text._cell_text > div.area_text > div:nth-child(2) > div > span:nth-child(1)')
-for i in user_id:
-    print(i.text)
-
-comment_date = 
-product_option
-comment_body
-product_rate
-comment_likes
-"""
 driver.quit()
